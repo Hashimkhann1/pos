09aeb0b26dea6886d872825e2f329bc4f56da520
Shahabmustafa <shahabmustafa57@gmail.com>
2024-11-01 22:46:31 +0500

    in this file i add app theme

--- lib/res/theme/light_theme.dart
+++ lib/res/theme/light_theme.dart
@@ -8,12 +8,23 @@ class LightTheme{
   static ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: AppColor.white,
     useMaterial3: true,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppColor.white,
+    /// appbar theme
+    appBarTheme:  const AppBarTheme(
+      backgroundColor: AppColor.primaryColor,
+      titleTextStyle: TextStyle(
+        color: AppColor.white,
+        fontWeight: FontWeight.w500,
+        fontSize: 16
+      ),
+      iconTheme: IconThemeData(color: AppColor.white),
     ),
+
+    /// drawer theme
     drawerTheme: const DrawerThemeData(
       backgroundColor: AppColor.white,
     ),
+
+    /// extension tile theme
     expansionTileTheme: ExpansionTileThemeData(
       collapsedIconColor: AppColor.primaryColor,
       shape: UnderlineInputBorder(
@@ -21,6 +32,8 @@ class LightTheme{
         borderSide: BorderSide.none,
       ),
     ),
+
+    /// text input decoration theme
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
@@ -43,13 +56,49 @@ class LightTheme{
         ),
       ),
       hintStyle: const TextStyle(
-        fontWeight: FontWeight.w600,
-        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        fontSize: 12,
       ),
       suffixIconColor: AppColor.primaryColor,
       prefixIconColor: AppColor.primaryColor,
       contentPadding: const EdgeInsets.symmetric(
         vertical: 15.0, horizontal: 20.0, // Padding inside the textfield
+      ),
+    ),
+
+    /// floating action button theme
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: AppColor.primaryColor,
+      foregroundColor: AppColor.white,
+      shape: UnderlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+
+    /// dialog theme
+    dialogTheme: DialogTheme(
+      titleTextStyle: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        color: AppColor.white,
+      ),
+      shape: UnderlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+    ),
+
+    /// elevation button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          color: AppColor.white,
+        ),
+        foregroundColor: AppColor.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        backgroundColor: AppColor.primaryColor,
+        minimumSize: Size(100, 40)
       ),
     ),
   );
