b27649fbe2dece82939c192277b770c559c43bcf
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-11 11:32:09 +0500

    firebase is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+// <<<<<<< HEAD
 import 'package:pos/res/root/root.dart';
+// =======
+// >>>>>>> c709d29 (initial view)
 
 void main() {
   runApp(const MyApp());
 }
 
-
+// <<<<<<< HEAD
+// =======
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: Scaffold()
+//     );
+//   }
+// }
+// >>>>>>> c709d29 (initial view)
+//
