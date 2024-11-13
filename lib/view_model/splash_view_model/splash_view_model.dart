fa82dbec8db19f0700682509b84892cee4954d80
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-13 21:54:49 +0500

    getting store data

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pos/view/authentication/login_view.dart';
+import 'package:pos/view_model/get_store_data_view_model/get_store_data_view_model.dart';
 
 import '../../view/branch/sale_invoice/sale_invoice_view.dart';
 
@@ -12,6 +13,7 @@ class SplashViewModel {
 
     Future.delayed(Duration(seconds: 1), () {
       if(_auth.currentUser != null){
+        GetStoreDataViewModel().getStoreData(context);
         Navigator.push(context, MaterialPageRoute(builder: (context) => SaleInvoiceView()));
       }else{
         Navigator.push(context, MaterialPageRoute(builder: (context) => LoginView()));
