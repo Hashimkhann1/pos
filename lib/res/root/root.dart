3c5edd99cd3119d8af5267b4b942841ad6aa12e7
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-14 22:37:25 +0500

    updated

--- lib/res/root/root.dart
+++ lib/res/root/root.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pos/res/theme/light_theme.dart';
 import 'package:pos/view/splash_view/splash_view.dart';
 import 'package:pos/view_model/bloc/loading_bloc/loading_bloc/loading_bloc.dart';
+import 'package:pos/view_model/bloc/store_data_bloc/store_data_bloc/store_data_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider(create: (_) => LoadingBloc())
+          BlocProvider(create: (_) => LoadingBloc()),
+          BlocProvider(create: (_) => StoreDataBloc())
         ],
         child: MaterialApp(
           title: 'Point of Sale',
