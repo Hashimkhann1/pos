e0461535e9b5f55f37b507e68e9d956f47924274
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-11-14 22:45:56 +0500

    add and remove cateory method is writed in this file and working on it too

--- lib/view_model/add_category_view_model/add_category_view_model.dart
+++ lib/view_model/add_category_view_model/add_category_view_model.dart
@@ -6,6 +6,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pos/view_model/bloc/loading_bloc/loading_bloc/loading_bloc.dart';
+import 'package:pos/view_model/bloc/loading_bloc/loading_bloc_event/loading_bloc_event.dart';
+import 'package:pos/view_model/bloc/store_data_bloc/store_data_bloc/store_data_bloc.dart';
+import 'package:pos/view_model/bloc/store_data_bloc/store_data_event/store_data_event.dart';
 
 class AddCategoryViewModel {
 
@@ -15,15 +20,19 @@ class AddCategoryViewModel {
 
   // add product category to firebase
   Future<void> addCategory(BuildContext context , String categoryName) async {
+    context.read<LoadingBloc>().add(SetLoading());
     try{
       await _firestore.doc(_auth.currentUser!.uid.toString()).update({
         'productsCategory': FieldValue.arrayUnion([categoryName.toString()])
       }).then((value) {
+        context.read<LoadingBloc>().add(SetLoading());
+        context.read<StoreDataBloc>().add(AddCategory(categoryName: categoryName));
         if(kDebugMode){
           print("Category is added >>>>>>>>>>> :)");
         }
       });
     }catch(error){
+      context.read<LoadingBloc>().add(SetLoading());
       if(kDebugMode){
         print("Error while Adding Category from AddCategoryViewModel $error");
       }
